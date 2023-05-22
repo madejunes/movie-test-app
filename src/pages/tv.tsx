c4e0835be6c93dd19bdfe7a4912bce0adbd1b9ef
made junes <made.junes@gmail.com>
2023-05-22 11:28:57 +0800

    render tv page response

--- src/pages/tv.tsx
+++ src/pages/tv.tsx
@@ -1,14 +1,59 @@
-import { APP_TITLE } from '@/utils/settings'
 import Head from 'next/head'
 
-export default function TvPage() {
+import ItemList from '@/features/shared/components/ItemList'
+
+import { APP_TITLE, TMDB_API_PREFIX } from '@/utils/settings'
+import { TvApiResponse } from '@/features/tv/tv'
+
+type MoviePageProps = {
+  topRatedTv: TvApiResponse,
+  popularTv: TvApiResponse
+}
+
+export default function MoviePage({topRatedTv, popularTv}: MoviePageProps) {
+  const topRated = topRatedTv.results
+  const popular = popularTv.results
+  topRated.forEach((tv) => (tv.contentType = 'tv'))
+  popular.forEach((tv) => (tv.contentType = 'tv'))
+
   return (
     <>
       <Head>
-        <title>TV | {APP_TITLE}</title>
+        <title>Movie | {APP_TITLE}</title>
       </Head>
-      <h1>Tv</h1>
-      <p>tv page ini</p>
+      <main>
+        <div className="flex justify-between mb-2">
+          <h2 className="text-2xl">Top Rated TV Series</h2>
+        </div>
+        <ItemList items={topRated} />
+
+        <div className="flex justify-between mb-2">
+          <h2 className="text-2xl">Popular TV Series</h2>
+        </div>
+        <ItemList items={popular} />
+      </main>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const topRatedTvRequest = fetch(
+    `${TMDB_API_PREFIX}tv/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+  ).then((res) => res.json())
+
+  const popularTvRequest = fetch(
+    `${TMDB_API_PREFIX}tv/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+  ).then((res) => res.json())
+
+  const [topRatedTv, popularTv] = await Promise.all([
+    topRatedTvRequest,
+    popularTvRequest
+  ])
+
+  return {
+    props: {
+      topRatedTv,
+      popularTv
+    },
+  }
 }
